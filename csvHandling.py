c3daf4823c2a102a3942dea5d87e9e7ee5827a4f
Jasnoor Aulakh <jasgaming02singh@gmail.com>
2023-11-05 10:05:51 -0600

    Update csvHandling.py
    Add delimiter to df to avoid null issues in data columns

--- csvHandling.py
+++ csvHandling.py
@@ -12,6 +12,7 @@ np.set_printoptions(precision=3, suppress=True)
 
 dataFrame1 = pd.read_csv(
     "dataSet.csv",
+    delimiter='\t',
     names=["ID", "GrossMonthlyIncome", "CreditCardPayment", "CarPayment", "StudentLoanPayments",
            "AppraisedValue", "DownPayment", "LoanAmount", "MonthlyMortgagePayment", "CreditScore"]
 )
